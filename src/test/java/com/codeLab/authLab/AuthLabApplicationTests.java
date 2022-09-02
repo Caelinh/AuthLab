0c175414ef4a8cc84e3f94eea4f288f6a3f47c9f
Caelinh <Caelinhargrave@gmail.com>
2022-09-02 01:09:06 -0500

    initial push

---
+++ src/test/java/com/codeLab/authLab/AuthLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeLab.authLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
